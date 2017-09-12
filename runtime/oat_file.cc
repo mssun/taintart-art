659a7dccd73c6b67e2d8bf56c84c09cbe69215c1
Calin Juravle <calin@google.com>
2017-09-12 00:54:26 -0700

    Add check that classpath is up to date to getDexOptNeeded
    Extend getDexOptNeeded to factor into the decision the expected class
    loader context. If the context does not match, oat file assistant and
    dexoptanalyzer will advise kDex2OatFromScratch.
    
    Note that this does not currently extend the java side
    DexFile.getDexOptNeeded. The calls coming from the java side will continue
    to ignore the classpath checks by passing null as the class loader
    context.
    
    Bug: 62269291
    Test: m test-art-host
    Change-Id: Ia01728c06810e418bbcbfe2a774d1f904d2525ba

--- runtime/oat_file.cc
+++ runtime/oat_file.cc
@@ -1619,6 +1619,10 @@ CompilerFilter::Filter OatFile::GetCompilerFilter() const {
   return GetOatHeader().GetCompilerFilter();
 }
 
+std::string OatFile::GetClassLoaderContext() const {
+  return GetOatHeader().GetStoreValueByKey(OatHeader::kClassPathKey);
+};
+
 OatFile::OatClass OatFile::FindOatClass(const DexFile& dex_file,
                                         uint16_t class_def_idx,
                                         bool* found) {
