d9aff1372e6a2f573b14d124f3d6f5305bdc6080
Alex Light <allight@google.com>
2017-10-31 22:30:05 +0000

    Revert "Revert "Prevent races with GC when transferring objects between threads""
    This reverts commit e5179ce0ca8becf34ba6e7b2f3988874fe647c26.
    
    Reason for revert: Fixed issues with:
      Checkpoint flag set without pending checkpoint in parent CL.
    
    Bug: 67838964
    Test: ./test.py --host -j50
    
    Change-Id: I7622f9c18866b58ee3cbd9f4fe38a29b2cf84a88

--- runtime/verifier/reg_type_cache.h
+++ runtime/verifier/reg_type_cache.h
@@ -61,7 +61,7 @@ static constexpr size_t kDefaultArenaBitVectorBytes = 8;
 
 class RegTypeCache {
  public:
-  explicit RegTypeCache(bool can_load_classes, ScopedArenaAllocator& allocator);
+  RegTypeCache(bool can_load_classes, ScopedArenaAllocator& allocator, bool can_suspend = true);
   ~RegTypeCache();
   static void Init() REQUIRES_SHARED(Locks::mutator_lock_) {
     if (!RegTypeCache::primitive_initialized_) {
