3e0c5170c87b39c88c7f3b5fbd7b07e60547bfd7
Mathieu Chartier <mathieuc@google.com>
2017-11-12 12:58:40 -0800

    Add logic for calculating offsets during writing
    Delete all the offset related layout logic, move the logic to
    writing. Layout is now controlled by changing the order of the data
    vectors.
    
    Calculate all offsets during writing instead of reading. This allows
    more flexible layout optimizations without needing to be as careful
    about offsets in other sections changing.
    
    Maintained logic for using existing offsets in the case where
    changing the layout is not required.
    
    Areas to improve:
    How to size the memmap, do we want 2 passes? Currently the algorithm
    reserves 10% extra space in case required. This is sufficient for top
    1000 apps in the play store. Will consider a two pass approach later.
    
    Bug: 63756964
    Bug: 68948395
    Bug: 68867570
    Bug: 68864106
    Bug: 68864168
    
    Test: test/testrunner/testrunner.py --host -j40
    Test: test-art-host-gtest
    Test: Dexlayout speed profile top 1000 apps
    
    Change-Id: I7dee869da3a010559547f8cfdf93e9aa4c3f47ff

--- dexlayout/dex_writer.h
+++ dexlayout/dex_writer.h
@@ -19,56 +19,119 @@
 #ifndef ART_DEXLAYOUT_DEX_WRITER_H_
 #define ART_DEXLAYOUT_DEX_WRITER_H_
 
+#include <functional>
+
 #include "base/unix_file/fd_file.h"
 #include "cdex/compact_dex_level.h"
 #include "dex_ir.h"
 #include "mem_map.h"
 #include "os.h"
 
+#include <queue>
+
 namespace art {
+
+class DexLayout;
+class DexLayoutHotnessInfo;
+
+struct MapItem {
+  // Not using DexFile::MapItemType since compact dex and standard dex file may have different
+  // sections.
+  MapItem() = default;
+  MapItem(uint32_t type, uint32_t size, uint32_t offset)
+      : type_(type), size_(size), offset_(offset) { }
+
+  // Sort by decreasing order since the priority_queue puts largest elements first.
+  bool operator>(const MapItem& other) const {
+    return offset_ > other.offset_;
+  }
+
+  uint32_t type_ = 0u;
+  uint32_t size_ = 0u;
+  uint32_t offset_ = 0u;
+};
+
+class MapItemQueue : public
+    std::priority_queue<MapItem, std::vector<MapItem>, std::greater<MapItem>> {
+ public:
+  void AddIfNotEmpty(const MapItem& item);
+};
 
 class DexWriter {
  public:
-  DexWriter(dex_ir::Header* header, MemMap* mem_map) : header_(header), mem_map_(mem_map) {}
+  DexWriter(dex_ir::Header* header,
+            MemMap* mem_map,
+            DexLayout* dex_layout,
+            bool compute_offsets)
+      : header_(header),
+        mem_map_(mem_map),
+        dex_layout_(dex_layout),
+        compute_offsets_(compute_offsets) {}
 
-  static void Output(dex_ir::Header* header, MemMap* mem_map, CompactDexLevel compact_dex_level);
+  static void Output(dex_ir::Header* header,
+                     MemMap* mem_map,
+                     DexLayout* dex_layout,
+                     bool compute_offsets,
+                     CompactDexLevel compact_dex_level);
 
   virtual ~DexWriter() {}
 
  protected:
   void WriteMemMap();
 
-  size_t Write(const void* buffer, size_t length, size_t offset);
-  size_t WriteSleb128(uint32_t value, size_t offset);
-  size_t WriteUleb128(uint32_t value, size_t offset);
-  size_t WriteEncodedValue(dex_ir::EncodedValue* encoded_value, size_t offset);
-  size_t WriteEncodedValueHeader(int8_t value_type, size_t value_arg, size_t offset);
-  size_t WriteEncodedArray(dex_ir::EncodedValueVector* values, size_t offset);
-  size_t WriteEncodedAnnotation(dex_ir::EncodedAnnotation* annotation, size_t offset);
-  size_t WriteEncodedFields(dex_ir::FieldItemVector* fields, size_t offset);
-  size_t WriteEncodedMethods(dex_ir::MethodItemVector* methods, size_t offset);
+  size_t Write(const void* buffer, size_t length, size_t offset) WARN_UNUSED;
+  size_t WriteSleb128(uint32_t value, size_t offset) WARN_UNUSED;
+  size_t WriteUleb128(uint32_t value, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedValue(dex_ir::EncodedValue* encoded_value, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedValueHeader(int8_t value_type, size_t value_arg, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedArray(dex_ir::EncodedValueVector* values, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedAnnotation(dex_ir::EncodedAnnotation* annotation, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedFields(dex_ir::FieldItemVector* fields, size_t offset) WARN_UNUSED;
+  size_t WriteEncodedMethods(dex_ir::MethodItemVector* methods, size_t offset) WARN_UNUSED;
 
-  void WriteStrings();
-  void WriteTypes();
-  void WriteTypeLists();
-  void WriteProtos();
-  void WriteFields();
-  void WriteMethods();
-  void WriteEncodedArrays();
-  void WriteAnnotations();
-  void WriteAnnotationSets();
-  void WriteAnnotationSetRefs();
-  void WriteAnnotationsDirectories();
-  void WriteDebugInfoItems();
-  void WriteCodeItems();
-  void WriteClasses();
-  void WriteCallSites();
-  void WriteMethodHandles();
-  void WriteMapItem();
+  // Header and id section
   virtual void WriteHeader();
+  // reserve_only means don't write, only reserve space. This is required since the string data
+  // offsets must be assigned.
+  uint32_t WriteStringIds(uint32_t offset, bool reserve_only);
+  uint32_t WriteTypeIds(uint32_t offset);
+  uint32_t WriteProtoIds(uint32_t offset, bool reserve_only);
+  uint32_t WriteFieldIds(uint32_t offset);
+  uint32_t WriteMethodIds(uint32_t offset);
+  uint32_t WriteClassDefs(uint32_t offset, bool reserve_only);
+  uint32_t WriteCallSiteIds(uint32_t offset, bool reserve_only);
+
+  uint32_t WriteEncodedArrays(uint32_t offset);
+  uint32_t WriteAnnotations(uint32_t offset);
+  uint32_t WriteAnnotationSets(uint32_t offset);
+  uint32_t WriteAnnotationSetRefs(uint32_t offset);
+  uint32_t WriteAnnotationsDirectories(uint32_t offset);
+
+  // Data section.
+  uint32_t WriteDebugInfoItems(uint32_t offset);
+  uint32_t WriteCodeItems(uint32_t offset, bool reserve_only);
+  uint32_t WriteTypeLists(uint32_t offset);
+  uint32_t WriteStringDatas(uint32_t offset);
+  uint32_t WriteClassDatas(uint32_t offset);
+  uint32_t WriteMethodHandles(uint32_t offset);
+  uint32_t WriteMapItems(uint32_t offset, MapItemQueue* queue);
+  uint32_t GenerateAndWriteMapItems(uint32_t offset);
+
+  // Process an offset, if compute_offset is set, write into the dex ir item, otherwise read the
+  // existing offset and use that for writing.
+  void ProcessOffset(uint32_t* const offset, dex_ir::Item* item) {
+    if (compute_offsets_) {
+      item->SetOffset(*offset);
+    } else {
+      // Not computing offsets, just use the one in the item.
+      *offset = item->GetOffset();
+    }
+  }
 
   dex_ir::Header* const header_;
   MemMap* const mem_map_;
+  DexLayout* const dex_layout_;
+  bool compute_offsets_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(DexWriter);
