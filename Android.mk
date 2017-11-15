cf053a57cad0cb4f3083e41f5e0617bffeba6cb1
Nicolas Geoffray <ngeoffray@google.com>
2017-11-15 15:10:09 +0000

    Add crash_dump to the list of golem unbundled dependencies.
    As we're compiling libbacktrace, we also need to compile
    crash_dump.
    
    Test: m -j32 build-art-unbundled-golem
    Change-Id: I08a9c6406440cc47585d26a770852d19fe098822

--- Android.mk
+++ Android.mk
@@ -484,7 +484,7 @@ build-art-host-golem: build-art-host \
 ########################################################################
 # Phony target for building what go/lem requires for syncing /system to target.
 .PHONY: build-art-unbundled-golem
-build-art-unbundled-golem: art-runtime linker oatdump $(TARGET_CORE_JARS)
+build-art-unbundled-golem: art-runtime linker oatdump $(TARGET_CORE_JARS) crash_dump
 
 ########################################################################
 # Rules for building all dependencies for tests.
