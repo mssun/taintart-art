59484b9fbbfd79cf485fbcdddffe35c706c5f70d
Andreas Gampe <agampe@google.com>
2018-02-14 14:00:46 -0800

    ART: Refactor run-test 983 dex verification
    Split the source for use in run-tests (using libdexfile) and
    CTS (using slicer). In preparation for compilation with the
    NDK.
    
    Test: m test-art-host
    Test: m cts
    Change-Id: Ie514445d90c77391ec685b53726f1f9dd50f1991

--- test/983-source-transform-verify/source_transform.cc
+++ test/983-source-transform-verify/source_transform.cc
@@ -14,24 +14,13 @@
  * limitations under the License.
  */
 
-#include <inttypes.h>
+#include "source_transform.h"
 
-#include <cstdio>
-#include <cstring>
-#include <iostream>
-#include <sstream>
-#include <vector>
+#include "jni.h"
 
 #include "android-base/stringprintf.h"
-#include "jni.h"
 #include "jvmti.h"
 #include "scoped_local_ref.h"
-
-#include "dex/code_item_accessors-inl.h"
-#include "dex/dex_file_loader.h"
-#include "dex/dex_file.h"
-#include "dex/dex_file_loader.h"
-#include "dex/dex_instruction.h"
 
 // Test infrastructure
 #include "jvmti_helper.h"
@@ -42,13 +31,12 @@ namespace Test983SourceTransformVerify {
 
 constexpr bool kSkipInitialLoad = true;
 
-static void Println(JNIEnv* env, std::ostringstream msg_stream) {
-  std::string msg = msg_stream.str();
+static void Println(JNIEnv* env, const char* msg) {
   ScopedLocalRef<jclass> test_klass(env, env->FindClass("art/Test983"));
   jmethodID println_method = env->GetStaticMethodID(test_klass.get(),
                                                     "doPrintln",
                                                     "(Ljava/lang/String;)V");
-  ScopedLocalRef<jstring> data(env, env->NewStringUTF(msg.c_str()));
+  ScopedLocalRef<jstring> data(env, env->NewStringUTF(msg));
   env->CallStaticVoidMethod(test_klass.get(), println_method, data.get());
 }
 
@@ -68,58 +56,12 @@ void JNICALL CheckDexFileHook(jvmtiEnv* jvmti_env ATTRIBUTE_UNUSED,
     // repeatable we only care about things that come from RetransformClasses.
     return;
   }
-  Println(env, std::ostringstream() << "Dex file hook for " << name);
+  Println(env, android::base::StringPrintf("Dex file hook for %s", name).c_str());
   if (IsJVM()) {
     return;
   }
 
-  // Due to b/72402467 the class_data_len might just be an estimate.
-  CHECK_GE(static_cast<size_t>(class_data_len), sizeof(DexFile::Header));
-  const DexFile::Header* header = reinterpret_cast<const DexFile::Header*>(class_data);
-  uint32_t header_file_size = header->file_size_;
-  CHECK_LE(static_cast<jint>(header_file_size), class_data_len);
-  class_data_len = static_cast<jint>(header_file_size);
-
-  const DexFileLoader dex_file_loader;
-  std::string error;
-  std::unique_ptr<const DexFile> dex(dex_file_loader.Open(class_data,
-                                                          class_data_len,
-                                                          "fake_location.dex",
-                                                          /*location_checksum*/ 0,
-                                                          /*oat_dex_file*/ nullptr,
-                                                          /*verify*/ true,
-                                                          /*verify_checksum*/ true,
-                                                          &error));
-  if (dex.get() == nullptr) {
-    Println(env, std::ostringstream() << "Failed to verify dex file for "
-                                      << name << " because " << error);
-    return;
-  }
-  for (uint32_t i = 0; i < dex->NumClassDefs(); i++) {
-    const DexFile::ClassDef& def = dex->GetClassDef(i);
-    const uint8_t* data_item = dex->GetClassData(def);
-    if (data_item == nullptr) {
-      continue;
-    }
-    for (ClassDataItemIterator it(*dex, data_item); it.HasNext(); it.Next()) {
-      if (!it.IsAtMethod() || it.GetMethodCodeItem() == nullptr) {
-        continue;
-      }
-      for (const DexInstructionPcPair& pair :
-          art::CodeItemInstructionAccessor(*dex, it.GetMethodCodeItem())) {
-        const Instruction& inst = pair.Inst();
-        int forbiden_flags = (Instruction::kVerifyError | Instruction::kVerifyRuntimeOnly);
-        if (inst.Opcode() == Instruction::RETURN_VOID_NO_BARRIER ||
-            (inst.GetVerifyExtraFlags() & forbiden_flags) != 0) {
-          Println(env, std::ostringstream() << "Unexpected instruction found in "
-                                            << dex->PrettyMethod(it.GetMemberIndex())
-                                            << " [Dex PC: 0x" << std::hex << pair.DexPc()
-                                            << std::dec << "] : " << inst.DumpString(dex.get()));
-          continue;
-        }
-      }
-    }
-  }
+  VerifyClassData(class_data_len, class_data);
 }
 
 // Get all capabilities except those related to retransformation.
