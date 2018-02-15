d6f914ea4bd09f2d60db2783f7a3fd93d3ec0afb
Nicolas Geoffray <ngeoffray@google.com>
2018-02-15 13:56:50 +0000

    Full-stack integrity: check vdex contents.
    For priv-apps, if a vdex on /data has dex code, it must
    be because the APK has compressed dex code.
    
    Fall back to APK if not.
    
    For test convenience, update --copy-dex-files to also
    take a "always" variant.
    
    bug: 30972906
    Test: 677-fsi
    
    Change-Id: I49f0eab46650145316a86de2971a3b9c140bcff7

--- dex2oat/linker/oat_writer.cc
+++ dex2oat/linker/oat_writer.cc
@@ -643,7 +643,7 @@ bool OatWriter::WriteAndOpenDexFiles(
     SafeMap<std::string, std::string>* key_value_store,
     bool verify,
     bool update_input_vdex,
-    bool copy_dex_files,
+    CopyOption copy_dex_files,
     /*out*/ std::vector<std::unique_ptr<MemMap>>* opened_dex_files_map,
     /*out*/ std::vector<std::unique_ptr<const DexFile>>* opened_dex_files) {
   CHECK(write_state_ == WriteState::kAddingDexFileSources);
@@ -3343,13 +3343,13 @@ bool OatWriter::RecordOatDataOffset(OutputStream* out) {
 bool OatWriter::WriteDexFiles(OutputStream* out,
                               File* file,
                               bool update_input_vdex,
-                              bool copy_dex_files) {
+                              CopyOption copy_dex_files) {
   TimingLogger::ScopedTiming split("Write Dex files", timings_);
 
   vdex_dex_files_offset_ = vdex_size_;
 
   // If extraction is enabled, only do it if not all the dex files are aligned and uncompressed.
-  if (copy_dex_files) {
+  if (copy_dex_files == CopyOption::kOnlyIfCompressed) {
     extract_dex_files_into_vdex_ = false;
     for (OatDexFile& oat_dex_file : oat_dex_files_) {
       if (!oat_dex_file.source_.IsZipEntry()) {
@@ -3362,7 +3362,10 @@ bool OatWriter::WriteDexFiles(OutputStream* out,
         break;
       }
     }
+  } else if (copy_dex_files == CopyOption::kAlways) {
+    extract_dex_files_into_vdex_ = true;
   } else {
+    DCHECK(copy_dex_files == CopyOption::kNever);
     extract_dex_files_into_vdex_ = false;
   }
 
