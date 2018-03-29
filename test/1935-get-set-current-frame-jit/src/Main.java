4f3d1cfbee45a27d5997c379f4eb7b7108224ca8
Nicolas Geoffray <ngeoffray@google.com>
2018-03-29 08:13:20 +0000

    Revert "Ensure that OSR still is possible with jvmti"
    Bug: 76226464
    
    Test doesn't work.
    
    This reverts commit b9ad26d1ed9146b89555d4333021f44eeb831f05.
    
    Change-Id: I3bdb60981e0e37457686cf9aeefb0f1103d485a0

--- test/1935-get-set-current-frame-jit/src/Main.java
+++ test/1935-get-set-current-frame-jit/src/Main.java
@@ -49,11 +49,9 @@ public class Main {
   public static class IntRunner implements Runnable {
     private volatile boolean continueBusyLoop;
     private volatile boolean inBusyLoop;
-    private final boolean expectOsr;
-    public IntRunner(boolean expectOsr) {
+    public IntRunner() {
       this.continueBusyLoop = true;
       this.inBusyLoop = false;
-      this.expectOsr = expectOsr;
     }
     public void run() {
       int TARGET = 42;
@@ -66,16 +64,9 @@ public class Main {
         Main.ensureJitCompiled(IntRunner.class, "run");
         i++;
       }
-      // We shouldn't be doing OSR since we are using JVMTI and the set prevents OSR.
+      // We shouldn't be doing OSR since we are using JVMTI and the get/set prevents OSR.
       // Set local will also push us to interpreter but the get local may remain in compiled code.
-      if (hasJit()) {
-        boolean inOsr = Main.isInOsrCode("run");
-        if (expectOsr && !inOsr) {
-          throw new Error("Expected to be in OSR but was not.");
-        } else if (!expectOsr && inOsr) {
-          throw new Error("Expected not to be in OSR but was.");
-        }
-      }
+      System.out.println("isInOsrCode? " + (hasJit() && Main.isInOsrCode("run")));
       reportValue(TARGET);
     }
     public void waitForBusyLoopStart() { while (!inBusyLoop) {} }
@@ -87,7 +78,7 @@ public class Main {
   public static void runGet() throws Exception {
     Method target = IntRunner.class.getDeclaredMethod("run");
     // Get Int
-    IntRunner int_runner = new IntRunner(true);
+    IntRunner int_runner = new IntRunner();
     Thread target_get = new Thread(int_runner, "GetLocalInt - Target");
     target_get.start();
     int_runner.waitForBusyLoopStart();
@@ -117,7 +108,7 @@ public class Main {
   public static void runSet() throws Exception {
     Method target = IntRunner.class.getDeclaredMethod("run");
     // Set Int
-    IntRunner int_runner = new IntRunner(false);
+    IntRunner int_runner = new IntRunner();
     Thread target_set = new Thread(int_runner, "SetLocalInt - Target");
     target_set.start();
     int_runner.waitForBusyLoopStart();
