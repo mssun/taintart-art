f28586390b055a5681e50617d729a3fa09792d9c
Alex Light <allight@google.com>
2018-04-02 11:28:50 -0700

    Revert^6 "Ensure that OSR still is possible with jvmti"
    The instrumentation uninstall could set methods to non-debuggable
    boot.oat code. This could cause events to be missed due to methods
    being inlined. We needed to change the path so that we would only have
    the JIT/interpreter replace methods. We do this by adding a new
    callback that can be used to determine if a method needs to be
    debuggable and being more careful about replacing code when this is
    true.
    
    This reverts commit 5f3005c8844d851d7d218b88b5f90d6c9083ce24.
    This unreverts commit b9ad26d1ed9146b89555d4333021f44eeb831f05.
    
    Reason for revert: Fixed issue causing CTS version of test 993 failure.
    
    Test: cts-tradefed run cts-dev CtsJvmtiRunTest993HostTestCases
    Test: ./test.py --host -j50 --all -t 993
    Test: ./test.py --host
    Test: while ./test/run-test --host --jit 1935; do; done
    Test: while ./test/run-test --host --jit --jvmti-redefine-stress 1935; do; done
    Test: am start --attach-agent -n com.example.android.displayingbitmaps/.ui.ImageGridActivity
          Run blur filter.
    Bug: 76226464
    Bug: 77306669
    
    Change-Id: I5068201a03f7613787c66981405499b6499c24e1

--- runtime/runtime_callbacks.h
+++ runtime/runtime_callbacks.h
@@ -130,6 +130,10 @@ class MethodInspectionCallback {
   // Note that '!IsMethodSafeToJit(m) implies IsMethodBeingInspected(m)'. That is that if this
   // method returns false IsMethodBeingInspected must return true.
   virtual bool IsMethodSafeToJit(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
+
+  // Returns true if we expect the method to be debuggable but are not doing anything unusual with
+  // it currently.
+  virtual bool MethodNeedsDebugVersion(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_) = 0;
 };
 
 class RuntimeCallbacks {
@@ -197,6 +201,11 @@ class RuntimeCallbacks {
   // (which implies that it is being Inspected). Returns true otherwise. If it returns false the
   // entrypoint should not be changed to JITed code.
   bool IsMethodSafeToJit(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_);
+
+  // Returns true if some MethodInspectionCallback indicates the method needs to use a debug
+  // version. This allows later code to set breakpoints or perform other actions that could be
+  // broken by some optimizations.
+  bool MethodNeedsDebugVersion(ArtMethod* method) REQUIRES_SHARED(Locks::mutator_lock_);
 
   void AddMethodInspectionCallback(MethodInspectionCallback* cb)
       REQUIRES_SHARED(Locks::mutator_lock_);
