7f40ae52e9de9badb534969fb116579aee11bb84
Alex Light <allight@google.com>
2018-04-18 15:19:15 -0700

    Move jit startup attempt to openjdkjvmti from adbconnection
    We were attempting to start the jit in adbconnection if it was not
    already running in order to improve the performance of debugging
    system_server and other similar processes. Since we have opened up the
    kArtTiVersion (0x70010200) environment to any agent (on userdebug/eng
    builds) it would be useful for these other agents also have the
    performance boost when running on these processes.
    
    Test: Build
    Test: adb root &&
          adb shell setenforce 0 &&
          adb forward tcp:12345 jdwp:`adb shell pidof system_server` &&
          jdb -attach localhost:12345;
          <Do things with debugger and device>
    
    Bug: 78119634
    Bug: 78195998
    Merged-In: Ibfe10b43dcd23d615c03c12af2625e2b0c54f6a4
    Change-Id: Ibfe10b43dcd23d615c03c12af2625e2b0c54f6a4
    (cherry picked from commit 60fbefcdf3578be9995a3be2b975b3998cdb430c)

--- adbconnection/adbconnection.cc
+++ adbconnection/adbconnection.cc
@@ -825,15 +825,6 @@ void AdbConnectionState::PerformHandshake() {
 
 void AdbConnectionState::AttachJdwpAgent(art::Thread* self) {
   art::Runtime* runtime = art::Runtime::Current();
-  if (runtime->GetJit() == nullptr && !runtime->GetInstrumentation()->IsForcedInterpretOnly()) {
-    // If we don't have a JIT we should try to start the jit for performance reasons.
-    runtime->CreateJit();
-    if (runtime->GetJit() == nullptr) {
-      LOG(WARNING) << "Could not start jit for debugging. This process might be quite slow as it "
-                   << "is running entirely in the interpreter. Try running 'setenforce 0' and "
-                   << "starting the debugging session over.";
-    }
-  }
   self->AssertNoPendingException();
   runtime->AttachAgent(/* JNIEnv */ nullptr,
                        MakeAgentArg(),
