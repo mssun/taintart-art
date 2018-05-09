d5aeadeac0debd90d0a931c1d41bee6a2d32b209
Calin Juravle <calin@google.com>
2018-05-08 18:23:24 -0700

    Clear the profile if we fail to add new methods or classes
    We may fail to add new data in the profile if it contains outdated data
    (e.g. if the dex files we profiled were updated in the meantime). If this
    happens, clear the profile to ensure we don't keep around useless data.
    
    Test: m test-art-host, 595-profiling-saving
    Bug: 77839992
    Bug: 79200824
    
    Change-Id: I238d56ba4eeec96a3464e42f164d0e798f555ec4

--- libprofile/profile/profile_compilation_info.cc
+++ libprofile/profile/profile_compilation_info.cc
@@ -96,9 +96,7 @@ ProfileCompilationInfo::ProfileCompilationInfo()
 
 ProfileCompilationInfo::~ProfileCompilationInfo() {
   VLOG(profiler) << Dumpable<MemStats>(allocator_.GetMemStats());
-  for (DexFileData* data : info_) {
-    delete data;
-  }
+  ClearData();
 }
 
 void ProfileCompilationInfo::DexPcData::AddClass(uint16_t dex_profile_idx,
@@ -2104,6 +2102,14 @@ bool ProfileCompilationInfo::ProfileFilterFnAcceptAll(
     const std::string& dex_location ATTRIBUTE_UNUSED,
     uint32_t checksum ATTRIBUTE_UNUSED) {
   return true;
+}
+
+void ProfileCompilationInfo::ClearData() {
+  for (DexFileData* data : info_) {
+    delete data;
+  }
+  info_.clear();
+  profile_key_map_.clear();
 }
 
 }  // namespace art
