b68ed2c708a5a3fcc8c2cec944c1080a6fbb88f5
Andreas Gampe <agampe@google.com>
2018-06-20 10:39:31 -0700

    ART: Ensure OatQuickMethodHeader is trivially copyable
    Remove empty constructor.
    
    Test: mmma art
    Change-Id: Ie58721299a6675797e9a525f4eb7e6df82abed50

--- runtime/oat_quick_method_header.h
+++ runtime/oat_quick_method_header.h
@@ -38,8 +38,6 @@ class PACKED(4) OatQuickMethodHeader {
                                 uint32_t fp_spill_mask,
                                 uint32_t code_size);
 
-  ~OatQuickMethodHeader();
-
   static OatQuickMethodHeader* FromCodePointer(const void* code_ptr) {
     uintptr_t code = reinterpret_cast<uintptr_t>(code_ptr);
     uintptr_t header = code - OFFSETOF_MEMBER(OatQuickMethodHeader, code_);
