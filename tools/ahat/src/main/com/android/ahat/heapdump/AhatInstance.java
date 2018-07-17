hopez13/art

Add --retained option to ahat
Which allows you to override what kind of references ahat considers to
retain an object.

Bug: 79131879
Test: m ahat-test, with new tests added.

Change-Id: I9bc2ed1aa0d0da27dd0a8a3b6456808c973fcdf9

tools/ahat/src/main/com/android/ahat/heapdump/AhatInstance.java
@@ -679,7 +679,7 @@ public abstract class AhatInstance implements Diffable<AhatInstance> {
           ref.ref.mReverseReferences = new ArrayList<AhatInstance>();
 
           for (Reference childRef : ref.ref.getReferences()) {
-            if (childRef.reachability.ordinal() <= reachability.ordinal()) {
+            if (childRef.reachability.isAsStrongAs(reachability)) {
               queue.add(childRef);
             } else {
               queues.get(childRef.reachability).add(childRef);
@@ -737,8 +737,8 @@ public abstract class AhatInstance implements Diffable<AhatInstance> {
     }
   }
 
-  Iterable<AhatInstance> getReferencesForDominators() {
-    return new DominatorReferenceIterator(getReferences());
+  Iterable<AhatInstance> getReferencesForDominators(Reachability retained) {
+    return new DominatorReferenceIterator(retained, getReferences());
   }
 
   void setDominator(AhatInstance dominator) {

tools/ahat/src/main/com/android/ahat/heapdump/DominatorReferenceIterator.java
@@ -21,14 +21,16 @@ import java.util.NoSuchElementException;
 
 /**
  * Reference iterator used for the dominators computation.
- * This visits only strong references.
+ * This visits only retained references.
  */
 class DominatorReferenceIterator implements Iterator<AhatInstance>,
                                             Iterable<AhatInstance> {
+  private final Reachability mRetained;
   private Iterator<Reference> mIter;
   private AhatInstance mNext;
 
-  public DominatorReferenceIterator(Iterable<Reference> iter) {
+  public DominatorReferenceIterator(Reachability retained, Iterable<Reference> iter) {
+    mRetained = retained;
     mIter = iter.iterator();
     mNext = null;
   }
@@ -37,7 +39,7 @@ class DominatorReferenceIterator implements Iterator<AhatInstance>,
   public boolean hasNext() {
     while (mNext == null && mIter.hasNext()) {
       Reference ref = mIter.next();
-      if (ref.reachability == Reachability.STRONG) {
+      if (ref.reachability.isAsStrongAs(mRetained)) {
         mNext = ref.ref;
       }
     }

tools/ahat/src/main/com/android/ahat/heapdump/Site.java
@@ -66,9 +66,9 @@ public class Site implements Diffable<Site> {
   private Site mBaseline;
 
   /**
-   * Summary information about instances allocated at a particular allocation
-   * site that are instances of a particular class and allocated on a
-   * particular heap.
+   * Summary information about retained instances allocated at a particular
+   * allocation site that are instances of a particular class and allocated on
+   * a particular heap.
    */
   public static class ObjectsInfo implements Diffable<ObjectsInfo> {
     /**
@@ -82,7 +82,7 @@ public class Site implements Diffable<Site> {
     public AhatClassObj classObj;   // May be null. Not sure why.
 
     /**
-     * The number of instances included in the summary.
+     * The number of retained instances included in the summary.
      */
     public long numInstances;
 
@@ -199,10 +199,11 @@ public class Site implements Diffable<Site> {
    * @param id - The smallest id that is allowed to be used for this site or
    * any of its children.
    * @param numHeaps - The number of heaps in the heap dump.
+   * @param retained the weakest reachability of instances to treat as retained.
    * @return An id larger than the largest id used for this site or any of its
    * children.
    */
-  long prepareForUse(long id, int numHeaps) {
+  long prepareForUse(long id, int numHeaps, Reachability retained) {
     mId = id++;
 
     // Count up the total sizes by heap.
@@ -211,9 +212,9 @@ public class Site implements Diffable<Site> {
       mSizesByHeap[i] = Size.ZERO;
     }
 
-    // Add all reachable objects allocated at this site.
+    // Add all retained objects allocated at this site.
     for (AhatInstance inst : mObjects) {
-      if (inst.isStronglyReachable()) {
+      if (inst.getReachability().isAsStrongAs(retained)) {
         AhatHeap heap = inst.getHeap();
         Size size = inst.getSize();
         ObjectsInfo info = getObjectsInfo(heap, inst.getClassObj());
@@ -225,7 +226,7 @@ public class Site implements Diffable<Site> {
 
     // Add objects allocated in child sites.
     for (Site child : mChildren) {
-      id = child.prepareForUse(id, numHeaps);
+      id = child.prepareForUse(id, numHeaps, retained);
       for (ObjectsInfo childInfo : child.mObjectsInfos) {
         ObjectsInfo info = getObjectsInfo(childInfo.heap, childInfo.classObj);
         info.numInstances += childInfo.numInstances;
@@ -303,7 +304,7 @@ public class Site implements Diffable<Site> {
    * {@link ObjectsInfo}. This method returns all the groups for this
    * allocation site.
    *
-   * @return all ObjectInfo summaries for instances allocated at this site
+   * @return all ObjectInfo summaries for retained instances allocated at this site
    */
   public List<ObjectsInfo> getObjectsInfos() {
     return mObjectsInfos;

mssun/taintart-art

tools/ahat/src/main/com/android/ahat/heapdump/AhatInstance.java
@@ -679,7 +679,7 @@ public abstract class AhatInstance implements Diffable<AhatInstance> {
           ref.ref.mReverseReferences = new ArrayList<AhatInstance>();
 
           for (Reference childRef : ref.ref.getReferences()) {
-            if (childRef.reachability.ordinal() <= reachability.ordinal()) {
+            if (childRef.reachability.notWeakerThan(reachability)) {
               queue.add(childRef);
             } else {
               queues.get(childRef.reachability).add(childRef);
@@ -737,8 +737,8 @@ public abstract class AhatInstance implements Diffable<AhatInstance> {
     }
   }
 
-  Iterable<AhatInstance> getReferencesForDominators() {
-    return new DominatorReferenceIterator(getReferences());
+  Iterable<AhatInstance> getReferencesForDominators(Reachability retained) {
+    return new DominatorReferenceIterator(retained, getReferences());
   }
 
   void setDominator(AhatInstance dominator) {

tools/ahat/src/main/com/android/ahat/heapdump/DominatorReferenceIterator.java
@@ -21,14 +21,16 @@ import java.util.NoSuchElementException;
 
 /**
  * Reference iterator used for the dominators computation.
- * This visits only strong references.
+ * This visits only retained references.
  */
 class DominatorReferenceIterator implements Iterator<AhatInstance>,
                                             Iterable<AhatInstance> {
+  private final Reachability mRetained;
   private Iterator<Reference> mIter;
   private AhatInstance mNext;
 
-  public DominatorReferenceIterator(Iterable<Reference> iter) {
+  public DominatorReferenceIterator(Reachability retained, Iterable<Reference> iter) {
+    mRetained = retained;
     mIter = iter.iterator();
     mNext = null;
   }
@@ -37,7 +39,7 @@ class DominatorReferenceIterator implements Iterator<AhatInstance>,
   public boolean hasNext() {
     while (mNext == null && mIter.hasNext()) {
       Reference ref = mIter.next();
-      if (ref.reachability == Reachability.STRONG) {
+      if (ref.reachability.notWeakerThan(mRetained)) {
         mNext = ref.ref;
       }
     }

tools/ahat/src/main/com/android/ahat/heapdump/Site.java
@@ -66,9 +66,9 @@ public class Site implements Diffable<Site> {
   private Site mBaseline;
 
   /**
-   * Summary information about instances allocated at a particular allocation
-   * site that are instances of a particular class and allocated on a
-   * particular heap.
+   * Summary information about retained instances allocated at a particular
+   * allocation site that are instances of a particular class and allocated on
+   * a particular heap.
    */
   public static class ObjectsInfo implements Diffable<ObjectsInfo> {
     /**
@@ -82,7 +82,7 @@ public class Site implements Diffable<Site> {
     public AhatClassObj classObj;   // May be null. Not sure why.
 
     /**
-     * The number of instances included in the summary.
+     * The number of retained instances included in the summary.
      */
     public long numInstances;
 
@@ -199,10 +199,11 @@ public class Site implements Diffable<Site> {
    * @param id - The smallest id that is allowed to be used for this site or
    * any of its children.
    * @param numHeaps - The number of heaps in the heap dump.
+   * @param retained the weakest reachability of instances to treat as retained.
    * @return An id larger than the largest id used for this site or any of its
    * children.
    */
-  long prepareForUse(long id, int numHeaps) {
+  long prepareForUse(long id, int numHeaps, Reachability retained) {
     mId = id++;
 
     // Count up the total sizes by heap.
@@ -211,9 +212,9 @@ public class Site implements Diffable<Site> {
       mSizesByHeap[i] = Size.ZERO;
     }
 
-    // Add all reachable objects allocated at this site.
+    // Add all retained objects allocated at this site.
     for (AhatInstance inst : mObjects) {
-      if (inst.isStronglyReachable()) {
+      if (inst.getReachability().notWeakerThan(retained)) {
         AhatHeap heap = inst.getHeap();
         Size size = inst.getSize();
         ObjectsInfo info = getObjectsInfo(heap, inst.getClassObj());
@@ -225,7 +226,7 @@ public class Site implements Diffable<Site> {
 
     // Add objects allocated in child sites.
     for (Site child : mChildren) {
-      id = child.prepareForUse(id, numHeaps);
+      id = child.prepareForUse(id, numHeaps, retained);
       for (ObjectsInfo childInfo : child.mObjectsInfos) {
         ObjectsInfo info = getObjectsInfo(childInfo.heap, childInfo.classObj);
         info.numInstances += childInfo.numInstances;
@@ -303,7 +304,7 @@ public class Site implements Diffable<Site> {
    * {@link ObjectsInfo}. This method returns all the groups for this
    * allocation site.
    *
-   * @return all ObjectInfo summaries for instances allocated at this site
+   * @return all ObjectInfo summaries for retained instances allocated at this site
    */
   public List<ObjectsInfo> getObjectsInfos() {
     return mObjectsInfos;