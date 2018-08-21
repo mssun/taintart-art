c702e34372e7120d3b99a42d9a226c6c036b5d17
Mathieu Chartier <mathieuc@google.com>
2018-08-21 21:04:58 +0000

    Revert "Avoid crash when ANDROID_DATA is not writeable."
    This reverts commit 00cbb4113bd646df142406fa080ecc3e7687091b.
    
    Reason for revert: Test is failing on 32 bit.
    
    Bug: 112848347
    Bug: 112827048
    Change-Id: Ic778038c9c51d4d9e3aa721a0ac2d164facc40e4

--- runtime/gc/space/image_space.cc
+++ runtime/gc/space/image_space.cc
@@ -1454,7 +1454,6 @@ class ImageSpace::BootImageLoader {
         has_system_(false),
         has_cache_(false),
         is_global_cache_(true),
-        dalvik_cache_exists_(false),
         dalvik_cache_(),
         cache_filename_() {
   }
@@ -1463,30 +1462,33 @@ class ImageSpace::BootImageLoader {
 
   void FindImageFiles() {
     std::string system_filename;
+    bool dalvik_cache_exists = false;
     bool found_image = FindImageFilenameImpl(image_location_.c_str(),
                                              image_isa_,
                                              &has_system_,
                                              &system_filename,
-                                             &dalvik_cache_exists_,
+                                             &dalvik_cache_exists,
                                              &dalvik_cache_,
                                              &is_global_cache_,
                                              &has_cache_,
                                              &cache_filename_);
-    DCHECK(!dalvik_cache_exists_ || !dalvik_cache_.empty());
+    DCHECK_EQ(dalvik_cache_exists, !dalvik_cache_.empty());
     DCHECK_EQ(found_image, has_system_ || has_cache_);
   }
 
   bool HasSystem() const { return has_system_; }
   bool HasCache() const { return has_cache_; }
 
-  bool DalvikCacheExists() const { return dalvik_cache_exists_; }
+  bool DalvikCacheExists() const { return !dalvik_cache_.empty(); }
   bool IsGlobalCache() const { return is_global_cache_; }
 
   const std::string& GetDalvikCache() const {
+    DCHECK(DalvikCacheExists());
     return dalvik_cache_;
   }
 
   const std::string& GetCacheFilename() const {
+    DCHECK(DalvikCacheExists());
     return cache_filename_;
   }
 
@@ -1615,7 +1617,6 @@ class ImageSpace::BootImageLoader {
   bool has_system_;
   bool has_cache_;
   bool is_global_cache_;
-  bool dalvik_cache_exists_;
   std::string dalvik_cache_;
   std::string cache_filename_;
 };
@@ -1768,7 +1769,7 @@ bool ImageSpace::LoadBootImage(
 
   // Step 3: We do not have an existing image in /system,
   //         so generate an image into the dalvik cache.
-  if (!loader.HasSystem() && loader.DalvikCacheExists()) {
+  if (!loader.HasSystem()) {
     std::string local_error_msg;
     if (!dex2oat_enabled) {
       local_error_msg = "Image compilation disabled.";
