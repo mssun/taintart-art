2337d53846d9d2e7f82f8cf64ff25585ad010339
Andreas Gampe <agampe@google.com>
2018-08-24 18:58:25 -0700

    ART: Restrict some checks in the verifier to P+
    Reinstate old behavior for apps targeting earlier releases.
    
    Bug: 111969862
    Test: m test-art-host
    Test: cts-tradefed run commandAndExit cts --m vm-tests-tf
    Change-Id: I207718d65f906817d20d5b32b53011530b616fab

--- runtime/verifier/method_verifier.cc
+++ runtime/verifier/method_verifier.cc
@@ -3675,9 +3675,11 @@ const RegType& MethodVerifier::ResolveClass(dex::TypeIndex class_idx) {
   // the access-checks interpreter. If result is primitive, skip the access check.
   //
   // Note: we do this for unresolved classes to trigger re-verification at runtime.
-  if (C == CheckAccess::kYes && result->IsNonZeroReferenceTypes()) {
+  if (C == CheckAccess::kYes &&
+      result->IsNonZeroReferenceTypes() &&
+      (api_level_ >= 28u || !result->IsUnresolvedTypes())) {
     const RegType& referrer = GetDeclaringClass();
-    if (!referrer.CanAccess(*result)) {
+    if ((api_level_ >= 28u || !referrer.IsUnresolvedTypes()) && !referrer.CanAccess(*result)) {
       Fail(VERIFY_ERROR_ACCESS_CLASS) << "(possibly) illegal class access: '"
                                       << referrer << "' -> '" << *result << "'";
     }
@@ -4737,7 +4739,7 @@ void MethodVerifier::VerifyISFieldAccess(const Instruction* inst, const RegType&
       DCHECK(!can_load_classes_ || self_->IsExceptionPending());
       self_->ClearException();
     }
-  } else {
+  } else if (api_level_ >= 28u) {
     // If we don't have the field (it seems we failed resolution) and this is a PUT, we need to
     // redo verification at runtime as the field may be final, unless the field id shows it's in
     // the same class.
