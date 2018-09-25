hopez13/art

Improving ART debugging and printing VmSize when OOM occurs
VmSize debugging information is added to quickly identify whether
the process address space or the system memory is insufficient

Test: 1 Manually trigger an OOM exception
      2 Checking whether the keyword VmSize exists in the OOM
      exception log
      3 art/test.py --host --64 -r

Signed-off-by: Wei Li <sirius.liwei@huawei.com>
Signed-off-by: Jinguang Dong <dongjinguang@huawei.com>

Change-Id: I0ea53f5136c06aba27712f95335ca722e7d30d95

libartbase/base/utils_test.cc
@@ -126,4 +126,10 @@ TEST_F(UtilsTest, BoundsCheckedCast) {
   EXPECT_EQ(BoundsCheckedCast<const uint64_t*>(buffer + 57, buffer, buffer_end), nullptr);
 }
 
+TEST_F(UtilsTest, GetProcessStatus) {
+  EXPECT_EQ("utils_test", GetProcessStatus("Name"));
+  EXPECT_EQ("R (running)", GetProcessStatus("State"));
+  EXPECT_EQ("<unknown>", GetProcessStatus("Group"));
+}
+
 }  // namespace art

mssun/taintart-art

libartbase/base/utils_test.cc
@@ -126,4 +126,12 @@ TEST_F(UtilsTest, BoundsCheckedCast) {
   EXPECT_EQ(BoundsCheckedCast<const uint64_t*>(buffer + 57, buffer, buffer_end), nullptr);
 }
 
+TEST_F(UtilsTest, GetProcessStatus) {
+  EXPECT_EQ("utils_test", GetProcessStatus("Name"));
+  EXPECT_EQ("R (running)", GetProcessStatus("State"));
+  EXPECT_EQ("<unknown>", GetProcessStatus("tate"));
+  EXPECT_EQ("<unknown>", GetProcessStatus("e"));
+  EXPECT_EQ("<unknown>", GetProcessStatus("Dummy"));
+}
+
 }  // namespace art