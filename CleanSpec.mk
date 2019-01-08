c5afd11a9ca9039865df980f6ebe6c651b8b527e
Roland Levillain <rpl@google.com>
2019-01-08 15:05:45 +0000

    Add a CleanSpec for the old Android Runtime APEX package.
    This package is no longer built, since the introduction of the
    "release" and "debug" packages (in CL
    https://android-review.googlesource.com/c/platform/art/+/826725).
    
    Test: Run "make nothing" and check that the file has been removed.
    Bug: 113373927
    Change-Id: Ie29df4d4064d958a55de504f19fd4321b881789f

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 The Android Open Source Project
+# Copyright (C) 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -49,6 +49,9 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libart_*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/STATIC_LIBRARIES/libartd_*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libart_*)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libartd_*)
+
+# Old Android Runtime APEX package, before the introduction of "release" and "debug" packages.
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/apex/com.android.runtime.apex)
 
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
